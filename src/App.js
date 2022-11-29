cfc4cb20302dba98cc6c8b5c8ff1511e14a7a2b6
sanjeev pratap <sanjeev892002@gmail.com>
2022-11-29 22:05:52 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './component/Navbar';
 import News from './component/News';
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 export default class App extends Component {
-  c = 'john';
+  pageSize=12;
   render() {
     return (
       <div>
-        <HashRouter>
+        <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route exact  key="general" path='/'element={<News pageSize={9} country="us" category="general" />} />
-            <Route exact  key="bussiness" path='/bussiness'element={<News pageSize={9} country="us" category="bussiness" />} />
-            <Route exact  key="entertainment" path='/entertainment'element={<News pageSize={9} country="us" category="entertainment" />} />
-            <Route exact  key="general" path='/general'element={<News pageSize={9} country="us" category="general" />} />
-            <Route exact  key="health" path='/health'element={<News pageSize={9} country="us" category="health" />} />
-            <Route exact  key="science" path='/science'element={<News pageSize={9} country="us" category="science" />} />
-            <Route exact  key="sports" path='/sports'element={<News pageSize={9} country="us" category="sports" />} />
-            <Route exact  key="technology" path='/technology'element={<News pageSize={9} country="us" category="technology" />} />
+            <Route exact   path='/'element={<News pageSize={9} country="in" category="general" key="general"/>} />
+            <Route exact   path='/bussiness'element={<News pageSize={9} country="in" category="bussiness" key="bussiness"/>} />
+            <Route exact   path='/entertainment'element={<News pageSize={9} country="in" category="entertainment" key="entertainment" />} />
+            <Route exact   path='/general'element={<News pageSize={9} country="in" category="general" key="general"/>} />
+            <Route exact   path='/health'element={<News pageSize={9} country="in" category="health" key="health" />} />
+            <Route exact   path='/science'element={<News pageSize={9} country="in" category="science" key="science"/>} />
+            <Route exact   path='/sports'element={<News pageSize={9} country="in" category="sports"  key="sports"/>} />
+            <Route exact   path='/technology'element={<News pageSize={9} country="in" category="technology" key="technology"/>} />
 
           </Routes>
 
-          {/* <News  pageSize={9} country="us" category="science"/> */}
+          {/* <News  pageSize={9} country="in" category="science"/> */}
 
-        </HashRouter>
+        </BrowserRouter>
         {/* Hello my first class based compponent {this.c} */}
       </div>
     )
